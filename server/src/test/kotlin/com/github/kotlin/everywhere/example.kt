55d979b987a535e1d49331cabbe0fe12df0365ca
Sung Min Park <vamf12@Sungs-MacBook-Pro.local>
2017-06-27 15:28:15 +0900

    new style api design.

--- server/src/test/kotlin/com/github/kotlin/everywhere/example.kt
+++ server/src/test/kotlin/com/github/kotlin/everywhere/example.kt
@@ -1,27 +1,88 @@
 package com.github.kotlin.everywhere
 
 import com.github.kotlin.everywhere.json.decode.Decoder
-import com.github.kotlin.everywhere.json.decode.Decoders.int
+import com.github.kotlin.everywhere.json.encode.Value
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+import com.github.kotlin.everywhere.json.decode.Decoders as d
+import com.github.kotlin.everywhere.json.encode.Encoders as e
 
-class Handler<P, R>(val decoder: Decoder<P>) {
+typealias Encoder<T> = (T) -> Value
+
+
+
+class Handler<P, R>(private val crate: Crate, private val name: String, private val decoder: Decoder<P>, private val encoder: Encoder<R>) {
+    private lateinit var handle: (P) -> R
+
     operator fun invoke(handle: (P) -> R): Unit {
+        this.handle = handle
+    }
+
+    class Delegate<P, R>(private val decoder: Decoder<P>, private val encoder: Encoder<R>) : ReadOnlyProperty<Crate, Handler<P, R>> {
+        internal var handler: Handler<P, R>? = null
+            private set
+
+        override fun getValue(thisRef: Crate, property: KProperty<*>): Handler<P, R> {
+            val handler = this.handler
+            if (handler != null) {
+                return handler
+            }
+            return Handler(thisRef, property.name, decoder, encoder).apply { this@Delegate.handler = this }
+        }
     }
 }
+
 
 abstract class Crate {
-    fun <P, R> f(decoder: Decoder<P>): Handler<P, R> {
-        return Handler(decoder)
+    private var handlerDelegates = listOf<Handler.Delegate<*, *>>()
+    private var createDelegates = listOf<Delegate<*>>()
+    private var parent = null as Parent?
+
+    fun <P, R> f(decoder: Decoder<P>, encoder: Encoder<R>): Handler.Delegate<P, R> {
+        val delegate = Handler.Delegate(decoder, encoder)
+        handlerDelegates += delegate
+        return delegate
     }
+
+    fun <T : Crate> f(crate: T): Delegate<T> {
+        return Delegate(crate).apply { createDelegates += this }
+    }
+
+    class Delegate<out T : Crate>(private val crate: T) : ReadOnlyProperty<Crate, T> {
+        private var initialized = false
+
+        override fun getValue(thisRef: Crate, property: KProperty<*>): T {
+            if (!initialized) {
+                crate.parent = Parent(property.name, thisRef)
+                initialized = true
+            }
+            return crate
+        }
+    }
+
+    private class Parent(val name: String, val crate: Crate)
 }
 
+class Calc : Crate() {
+    val halfIt by f(d.float, e.float)
+}
 
 class Root : Crate() {
-    val doubleIt = f<Int, Int>(int)
+    val doubleIt by f(d.int, e.int)
+    val calc by f(Calc())
 }
 
 fun Root.impl() {
     doubleIt {
         it * 2
+    }
+
+    calc.impl()
+}
+
+fun Calc.impl() {
+    halfIt {
+        it / 2
     }
 }
 
